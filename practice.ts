ba60e1c1e5a5d005ae253f556996de9f685a7a90
Subham <chromaticshubham26@gmail.com>
2026-03-16 03:44:47 +0530

    a small bug

--- practice.ts
+++ practice.ts
@@ -1,2 +1,5 @@
-import mongoose from "mongoose";
-
+import { NextResponse } from "next/server";
+import { User } from "./models/User";
+import { connectDB } from "./lib/mongodb";
+import bcrypt from "bcryptjs";
+import generateToken 
--- utils/auth.ts
+++ utils/auth.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 const JWT_TOKEN = process.env.JWT_TOKEN!
 
-export function getToken(userId: string){
+export function generateToken(userId: string){
     return jwt.sign({id: userId}, JWT_TOKEN, {
         expiresIn: "7d",
     })
